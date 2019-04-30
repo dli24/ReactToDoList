3c37ea935b57d7805be38d6a10acf30ba4ba19e0
dli24 <dli24@buffalo.edu>
2019-04-30 11:06:39 -0700

    Sprint1

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Header extends Component{
+    render(){
+        return(
+            <header>
+                <h1>Todo</h1>
+                <nav>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/todos'}>Todos</Link>
+                </nav>
+            </header>
+        )
+    };
+
+
+};
+
+export default Header;
---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class TodosContainer extends Component {
+    render(){
+        return(
+            <h2>
+                TodosContainer
+            </h2>
+
+        )
+
+
+    }
+}
+
+export default TodosContainer;
